7803506410621c55e10896a8f67b927c64a804d3
Richard O. Legendi <richard.legendi@gmail.com>
2012-05-26 23:45:15 +0200

    Added a simple test for standard specifications

---
+++ src/test/scala/org/scalatest/specs2/HelloWorldSpecTest.scala
@@ -0,0 +1,36 @@
+package org.scalatest.specs2
+
+import org.specs2._
+import org.scalatest.Filter
+import org.junit.runner.RunWith
+import org.scalatest.WrapWith
+import org.specs2.runner.JUnitRunner
+
+class HelloWorldSpec extends Specification { def is =
+
+  "This is a specification to check the 'Hello world' string"                 ^
+                                                                              p^
+  "The 'Hello world' string should"                                           ^
+    "contain 11 characters"                                                   ! e1^
+    "start with 'Hello'"                                                      ! e2^
+    "end with 'world'"                                                        ! e3^
+                                                                              end
+
+  def e1 = "Hello world" must have size(11)
+  def e2 = "Hello world" must startWith("Hello")
+  def e3 = "Hello world" must endWith("world")
+}
+
+@RunWith(classOf[JUnitRunner])
+@WrapWith(classOf[MutableSpec2Runner])
+class HelloWorldSpecTest extends Specification { def is =
+
+  "This is a specification for HelloWorldSpec as a standard specification"    ^
+                                                                              p^
+  "When executing 'HelloWorldSpec' the runner should"                         ^
+    "find 3 examples"                                                         ! threeExamplesFound^
+                                                                              end
+   lazy val noFilter = new Filter(None, Set.empty[String])
+
+   def threeExamplesFound = MutableSpec2Runner(classOf[HelloWorldSpec]).expectedTestCount(noFilter) must be_==(3)
+}
