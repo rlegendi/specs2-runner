0eba49b187795d970c4da2aa12e3555745c898e1
Richard O. Legendi <richard.legendi@gmail.com>
2012-05-26 19:37:04 +0200

    Added finder based on Chee Seng's `Spec1Finder` to search for spec2 unit specifications

--- src/main/scala/org/scalatest/specs2/Spec2Finder.scala
+++ src/main/scala/org/scalatest/specs2/Spec2Finder.scala
@@ -4,9 +4,16 @@ import org.scalatest.finders.AstNode
 import org.scalatest.finders.Selection
 import org.scalatest.specs.Spec1Finder
 
+/**
+ * Finder class that can find test cases in the format ("should", "can") { "in" { ... } }
+ *
+ * @author rlegendi
+ */
 class Spec2Finder extends Spec1Finder {
 
   // TODO Specifications with function 'is'? (scope..)
+  // TODO JUnitRunner in Specs2 searches for test extensively and it is well-tested
+  //      Can't we use that aproach instead of playing with AstNodes?
 
   override def find(node: AstNode): Option[Selection] = {
     super.find(node)
