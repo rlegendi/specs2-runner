12adee262b83f839e73a050065eb34b24b385f54
Richard O. Legendi <richard.legendi@gmail.com>
2012-05-31 22:38:40 +0200

    Added some documentation

--- src/main/scala/org/scalatest/specs2/Utils.scala
+++ src/main/scala/org/scalatest/specs2/Utils.scala
@@ -3,8 +3,24 @@ package org.scalatest.specs2
 import org.specs2.Specification
 import org.specs2.specification.SpecificationStructure
 
+/**
+ * A set of minimal utility functions.
+ *
+ * @author rlegendi
+ */
 object Utils {
 
+  /**
+   * Creates a nice suite name for the given specs2 specification instance.
+   *
+   * <p>
+   * The suite name is used to display on the <i>ScalaTest View</i>.
+   * </p>
+   *
+   * @param spec either a mutable (unit) specification or a standard specification;
+   * 				<i>cannot be null</i>
+   * @return a decorated version of the given <code>spec</code> (if applicable)
+   */
   def suiteNameFor(spec: SpecificationStructure): String = {
     if (null == spec) throw new IllegalArgumentException("spec == null")
 
@@ -14,6 +30,7 @@ object Utils {
     else if (spec.isInstanceOf[org.specs2.mutable.Specification])
       return baseName + " specs2 Unit Specification"
     else
+      // Nothing falls into this category jet, but a default value is always good to have
       return baseName + " specs2 Test"
   }
 
