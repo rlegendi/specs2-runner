5d78dd2b6ccd39d511fad4a5ed29a017cc5e6f68
Eric Torreborre <etorreborre@yahoo.com>
2012-06-01 09:36:51 +1000

    answered the questions in TODOs and proposed an alternative implementation strategy using a specs2 Notifier

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "specs2-runner"
  
 version := "0.1.0"
  
-scalaVersion := "2.9.1"
+scalaVersion := "2.9.1"`
 
 // If you use groupID %% artifactID % revision rather than groupID % artifactID % revision (the difference is the double %% after the groupID), sbt will add your project's Scala version to the artifact name. 
 // https://github.com/harrah/xsbt/wiki/Getting-Started-Library-Dependencies
--- src/main/scala/org/specs2/Specs2Bridge.scala
+++ src/main/scala/org/specs2/Specs2Bridge.scala
@@ -17,6 +17,7 @@ import org.specs2.execute.Failure
 import org.specs2.execute.Success
 import org.scalatest.Tracker
 import org.scalatest.events.TestStarting
+import reporter.Reporter
 import scala.reflect.NameTransformer
 import org.scalatest.events.MotionToSuppress
 import org.specs2.specification.ExecutedSpecStart
@@ -37,7 +38,7 @@ object Specs2Bridge {
     Classes.tryToCreateObject(className, printMessage, printStackTrace, loader)(m)
   }
 
-  def getContentFor(spec: SpecificationStructure): Fragments = spec.content;
+  implicit def getContentFor(spec: SpecificationStructure): Fragments = spec.content;
 
   def createExecuteSpecification(name: SpecName, fs: Seq[ExecutedFragment], args: Arguments = Arguments()): ExecutingSpecification =
     ExecutingSpecification.create(name, fs, args)
@@ -94,6 +95,7 @@ object Specs2Bridge {
            */
 
         // TODO Ask Eric: When do the DecoratedResults are used?
+        // ERIC: this is used to display the html table for a DataTable when reporting to html files
 
         result match {
           case f @ Failure(m, e, st, d) =>
