832e142422db0c36798298c1bcb2bfaebd412096
Richard O. Legendi <richard.legendi@gmail.com>
2012-06-01 21:57:01 +0200

    Added a few tests to check line numbering conversation

---
+++ src/test/scala/org/scalatest/specs2/ScalaTestNotifierUnitSpecTest.scala
@@ -0,0 +1,32 @@
+package org.scalatest.specs2
+
+import org.specs2.mutable.Specification
+import ScalaTestNotifier._
+import org.junit.runner.RunWith
+import org.scalatest.WrapWith
+import org.specs2.runner.JUnitRunner
+import org.scalatest.events.LineInFile
+
+@RunWith(classOf[JUnitRunner])
+@WrapWith(classOf[Spec2Runner])
+class ScalaTestNotifierUnitSpecTest extends Specification {
+  "The locator function" should {
+    "require non-null parameter" in {
+      loc(null) must throwA[IllegalArgumentException]
+    }
+
+    "return a proper location for a simple case" in {
+      val spec2Loc = "org.scalatest.specs2.Spec2Runner (Spec2Runner.scala:164)"
+      loc(spec2Loc) must be_==(Some(LineInFile(164, "Spec2Runner.scala")))
+    }
+
+    "return a proper location for a file with several spaces" in {
+      val spec2Loc = "org.scalatest.specs2.Spec2Runner ( Spec 2 Runner .scala :164)"
+      loc(spec2Loc) must be_==(Some(LineInFile(164, " Spec 2 Runner .scala ")))
+    }
+
+    "return None for any malformed input" in {
+      loc("Hit me baby one more time") must be_==(None)
+    }
+  }
+}
