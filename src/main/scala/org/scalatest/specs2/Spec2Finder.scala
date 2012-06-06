f844cd49abd6aca4eed30df6db33834854cfffce
Richard O. Legendi <richard.legendi@gmail.com>
2012-06-06 23:05:42 +0200

    Added a simple testcase for two specs in the same file

--- src/main/scala/org/scalatest/specs2/Spec2Finder.scala
+++ src/main/scala/org/scalatest/specs2/Spec2Finder.scala
@@ -14,6 +14,7 @@ class Spec2Finder extends Spec1Finder {
   // TODO Specifications with function 'is'? (scope..)
   // TODO JUnitRunner in Specs2 searches for test extensively and it is well-tested
   //      Can't we use that aproach instead of playing with AstNodes?
+  // TODO Test if it can find 2 tests in the same file (TwoSpecInSameFile.scala)
 
   override def find(node: AstNode): Option[Selection] = {
     super.find(node)
---
+++ src/test/scala/org/scalatest/specs2/TwoSpecInSameFile.scala
@@ -0,0 +1,27 @@
+package org.scalatest.specs2
+
+import org.specs2._
+import org.scalatest.Filter
+import org.junit.runner.RunWith
+import org.scalatest.WrapWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+@WrapWith(classOf[Spec2Runner])
+class FirsOfTwoSpecInSameFile extends Specification { def is =
+
+  "This is the first of two specs"                         ^
+    "and it should do something cool"                      ! e^
+                                                           end
+   def e = success
+}
+
+@RunWith(classOf[JUnitRunner])
+@WrapWith(classOf[Spec2Runner])
+class SecondOfTwoSpecInSameFile extends Specification { def is =
+
+  "This is the first of two specs"                         ^
+    "and it should also do something cool"                 ! e^
+                                                           end
+   def e = success
+}
