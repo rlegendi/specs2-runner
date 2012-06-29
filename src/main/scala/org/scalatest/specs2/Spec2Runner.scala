e65fb9fa3a83c1132a66d1f1c7e01e7d8a2d7fbd
Richard O. Legendi <richard.legendi@gmail.com>
2012-06-29 16:00:23 +0200

    Cleanup

--- src/main/scala/org/scalatest/specs2/Spec2Runner.scala
+++ src/main/scala/org/scalatest/specs2/Spec2Runner.scala
@@ -113,7 +113,7 @@ class Spec2Runner(specs2Class: Class[_ <: SpecificationStructure]) extends Suite
     require(reporter != null)
     require(filter != null)
 
-    // TODO Here we get where the whole thing starts, needs checking
+    // TODO Here we get where the whole thing starts, needs checking (report execution started?)
     new NotifierReporter {
       val notifier = new ScalaTestNotifier(spec2, args, tracker, reporter)
     }.report(spec2)(args)
--- src/main/scala/org/specs2/Specs2Bridge.scala
+++ src/main/scala/org/specs2/Specs2Bridge.scala
@@ -1,34 +1,8 @@
 package org.specs2
 
-import org.specs2.reflect.Classes
 import org.specs2.specification.Fragments
 import org.specs2.specification.SpecificationStructure
-import org.specs2.main.Arguments
-import org.specs2.specification.ExecutingSpecification
-import org.specs2.specification.ExecutingFragment
-import org.specs2.specification.SpecName
-import org.specs2.execute.Pending
-import org.specs2.specification.ExecutedResult
-import org.specs2.execute.DecoratedResult
-import org.specs2.execute.Skipped
-import org.scalatest.Reporter
-import org.specs2.execute.Failure
-import org.specs2.execute.Success
-import org.scalatest.Tracker
-import org.scalatest.events.TestStarting
-//import reporter.Reporter
-import scala.reflect.NameTransformer
-import org.scalatest.events.MotionToSuppress
-import org.specs2.specification.ExecutedSpecStart
-import org.specs2.specification.ExecutedSpecEnd
-import org.scalatest.events.TestSucceeded
-import org.scalatest.Suite
-import org.scalatest.ScalaTestBridge
-import org.scalatest.events.TestFailed
-import org.specs2.execute.Error
-import org.scalatest.events.TestPending
-import org.scalatest.specs2.Utils
-import org.specs2.specification.ExecutedFragment
+import org.specs2.reflect.Classes
 
 object Specs2Bridge {
   def tryToCreateSpecification[T <: AnyRef](specs2Class: Class[_ <: SpecificationStructure]): SpecificationStructure = {
